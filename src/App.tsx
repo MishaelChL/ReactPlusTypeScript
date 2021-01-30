f85c249127cf0f698ffdaa32d76bd163b73bdca4
MishaelChL <jose.mishael.chile@gmail.com>
2021-01-29 22:01:45 -0400

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
+
 function App() {
   return (
     <div className="App">
-      <h1>React + Typescript</h1>
+      <h1>React + TypeScript</h1>
+      <hr/>
+      <Counter/>
+
+      <Usuario/>
     </div>
   );
 }
